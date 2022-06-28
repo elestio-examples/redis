1d0340644328a38cb93a2aa526b3a7ff24ac5d2d
jbenguira <z51biz@gmail.com>
2022-06-28 20:00:23 +0300

    fixed ports and postInstall

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -1,4 +1,6 @@
+set -o allexport; source .env; set +o allexport;
+
 #register the local server in the web ui
 curl --header "Content-Type: application/json" \
- --request POST --data '{ "name": "local", "connectionType": "STANDALONE", "host": "'"$APP_IP"'","port": '"$APP_PORT"',"password": "'"$SOFTWARE_PASSWORD"'"}' \
- http://$APP_IP:$APP_PORT/api/instance/
+ --request POST --data '{ "name": "local", "connectionType": "STANDALONE", "host": "'"$REDIS_INTERNAL_IP"'","port": '"$REDIS_INTERNAL_PORT"',"password": "'"$SOFTWARE_PASSWORD"'"}' \
+ http://$INSIGHT_INTERNAL_IP:$INSIGHT_INTERNAL_PORT/api/instance/
