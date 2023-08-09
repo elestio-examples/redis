f98e228d044153758078733be0990d09cb9b0b46
Amit Shukla <53058928+amitshuklabag@users.noreply.github.com>
2023-08-09 17:50:17 +0530

    Update preInstall.sh

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -6,3 +6,11 @@ chown -R 1000:1000 ./insight
 
 mkdir -p ./data
 chown -R 1000:1000 ./data
+
+export $(xargs <.env)
+
+echo "redis-cli -h $DOMAIN -p  6379 -a $SOFTWARE_PASSWORD" > ./cli.sh;
+echo "redis-cli -h $DOMAIN -p  6379 -a $SOFTWARE_PASSWORD save" > ./save.sh;
+
+chmod +x ./cli.sh;
+chmod +x ./save.sh;
