893767759529927c71f5dc95d853b3f2caa14128
David Alimi <davealimi@gmail.com>
2024-03-28 17:23:32 +0200

    cancel SSL for fixing on dedicated server

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -15,13 +15,13 @@ curl --output /dev/null --header "Content-Type: application/json" \
  http://$redis_insight_target/api/instance/
 
 
-sleep 25s;
-docker-compose down
-sudo apt-get install openssl -y
-openssl genrsa -out ./data/redis.key 2048
-openssl req -new -key ./data/redis.key -out ./data/redis.csr -subj "/CN=${CNAME}"
-openssl x509 -req -days 365 -in ./data/redis.csr -signkey ./data/redis.key -out ./data/redis.crt
-cp ./data/redis.crt ./data/ca.crt 
-sleep 5s;
-sed -i 's/#--tls/--tls/' docker-compose.yml
-docker-compose up -d;
+# sleep 25s;
+# docker-compose down
+# sudo apt-get install openssl -y
+# openssl genrsa -out ./data/redis.key 2048
+# openssl req -new -key ./data/redis.key -out ./data/redis.csr -subj "/CN=${CNAME}"
+# openssl x509 -req -days 365 -in ./data/redis.csr -signkey ./data/redis.key -out ./data/redis.crt
+# cp ./data/redis.crt ./data/ca.crt 
+# sleep 5s;
+# sed -i 's/#--tls/--tls/' docker-compose.yml
+# docker-compose up -d;
